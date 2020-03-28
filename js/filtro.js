bda22f4db76c050c6d297479f90f6415a5f775b7
ItzMont <itz_mont09@outlook.com>
2020-03-27 20:29:33 -0600

    Añadido con git

---
+++ js/filtro.js
@@ -0,0 +1,11 @@
+//Realiza Filtro de las opciones en la tabla 
+function filtro(){
+        $(document).ready(function(){
+          $("#myInput").on("keyup", function() {
+            var value = $(this).val().toLowerCase();
+            $("#myTable tr").filter(function() {
+              $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+            });
+          });
+        });
+}
